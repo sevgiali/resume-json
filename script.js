ee46fa84bfd3190ec1156c7ee13abce9d4da5830
Sevgi Ali <38467611+sevgiali@users.noreply.github.com>
2023-03-09 20:36:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+let myResume = {
+  basics: {
+    name: "Sevgi Ali",
+    label: "Programmer",
+    email: "sevgiialii97@gmail.com",
+    phone: "+359 877 977 696",
+    summary: "A summary of Sevgi Ali",
+    location: {
+      city: "Plovdiv",
+      country: "Bulgaria",
+    },
+    profiles: [
+      {
+        network: "LinkedIn",
+        username: "sevgialiyy",
+        url: "https://www.linkedin.com/in/sevgialiyy/",
+      },
+    ],
+  },
+  work: [
+    {
+      name: "WebSpace LTD",
+      position: "Online Store Administrator",
+      startDate: "2021-11-01",
+      endDate: "2022-12-01",
+    },
+  ],
+  education: [
+    {
+      institution: "Plovdiv University PaiSii Hilendarski",
+      area: "Business IT",
+      studyType: "Bachelor",
+      startDate: "2016",
+      endDate: "2020",
+    },
+    {
+      institution: "Plovdiv University PaiSii Hilendarski",
+      area: "Software technologies with a specialization in systems with AI",
+      studyType: "Master",
+      startDate: "2020",
+      endDate: "2021",
+    },
+  ],
+  courses: [
+    {
+      name: "Design Fundamentals",
+      date: "2022",
+      issuer: "SoftUni",
+    },
+    {
+      name: "JS Essentials",
+      date: "2022",
+      issuer: "SoftUni",
+    },
+    {
+      name: "JS Advanced",
+      date: "2022",
+      issuer: "SoftUni",
+    },
+    {
+      name: "The Complete Javascript Course 2023 - From Zero to Expert!",
+      date: "2023",
+      issuer: "Udemy",
+    },
+  ],
+  skills: [
+    {
+      name: "Web Development",
+      level: "Мotivated beginner",
+      keywords: ["HTML", "CSS", "JavaScript", "OOP"],
+    },
+  ],
+  languages: [
+    {
+      language: "English",
+      fluency: "Native speaker",
+    },
+    {
+      language: "Turkish",
+      fluency: "Native speaker",
+    },
+    {
+      language: "Bulgarian",
+      fluency: "Native speaker",
+    },
+  ],
+  interests: [
+    {
+      name: "Healthy Lifestyle",
+      keywords: ["Training", "Hiking"],
+    },
+  ],
+};
+
+console.log(myResume);
